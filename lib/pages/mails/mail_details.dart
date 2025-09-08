40ee9d05cb0b8e285d6e1ec49e90e6b32d69b6c6
Brandon Guigo <brandon.guigo@gmail.com>
2025-09-08 16:11:11 +0200

    feat: display the content of an email
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/pages/mails/mail_details.dart
+++ lib/pages/mails/mail_details.dart
@@ -5,6 +5,7 @@ import 'package:ab_shared/utils/shortcuts.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:mail/components/avatars/mail_user_avatar.dart';
 import 'package:mail/models/mail/mail.dart';
 
@@ -60,17 +61,66 @@ class MailDetailScreenState extends ResponsiveState<MailDetailScreen> {
               ),
               child: ElevatedContainer(
                 padding: EdgeInsets.symmetric(
-                    horizontal: $constants.insets.xs,
+                    horizontal: $constants.insets.sm,
                     vertical: $constants.insets.sm),
                 child: Column(
                   children: [
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         MailUserAvatar(
                             value: widget.mail.getHeader("From"),
                             read: widget.mail.read),
+                        SizedBox(width: $constants.insets.sm),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            buildPeopleRow(
+                                "From", widget.mail.getHeader("From")),
+                            buildPeopleRow("To", widget.mail.getHeader("To")),
+                          ],
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                Jiffy.parseFromDateTime(widget.mail.createdAt!)
+                                    .yMd
+                                    .toString(),
+                                style: getTextTheme(context)
+                                    .bodySmall!
+                                    .copyWith(color: Colors.grey),
+                              ),
+                              Text(
+                                Jiffy.parseFromDateTime(widget.mail.createdAt!)
+                                    .Hm
+                                    .toString(),
+                                style: getTextTheme(context)
+                                    .bodySmall!
+                                    .copyWith(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        )
                       ],
-                    )
+                    ),
+                    SizedBox(height: $constants.insets.xs),
+                    Divider(),
+                    SizedBox(height: $constants.insets.xs),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: $constants.insets.sm,
+                        vertical: $constants.insets.xs,
+                      ),
+                      width: double.infinity,
+                      child: Text(
+                        getContent(),
+                        textAlign: TextAlign.left,
+                        style: getTextTheme(context).bodyMedium,
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -79,6 +129,36 @@ class MailDetailScreenState extends ResponsiveState<MailDetailScreen> {
           SizedBox(height: $constants.insets.lg),
         ],
       ),
+    );
+  }
+
+  String getContent() {
+    if (widget.mail.htmlContent != null && widget.mail.htmlContent != "") {
+      return widget.mail.htmlContent!;
+    } else if (widget.mail.textContent != null &&
+        widget.mail.textContent!.isNotEmpty) {
+      return widget.mail.textContent!;
+    } else {
+      return "No content";
+    }
+  }
+
+  Widget buildPeopleRow(String label, String value) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "$label: ",
+          style: getTextTheme(context).bodyMedium!.copyWith(color: Colors.grey),
+        ),
+        SizedBox(width: $constants.insets.xs),
+        Text(
+          value,
+          style: getTextTheme(context)
+              .bodyMedium!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 
