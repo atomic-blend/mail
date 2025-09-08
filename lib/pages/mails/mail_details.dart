807c61464bd1e7553f26ab122c4d579b63e04fb0
Brandon Guigo <brandon.guigo@gmail.com>
2025-09-08 15:50:04 +0200

    feat: add the mail detail screen
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

---
+++ lib/pages/mails/mail_details.dart
@@ -0,0 +1,94 @@
+import 'package:ab_shared/components/responsive_stateful_widget.dart';
+import 'package:ab_shared/components/widgets/elevated_container.dart';
+import 'package:ab_shared/utils/constants.dart';
+import 'package:ab_shared/utils/shortcuts.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mail/components/avatars/mail_user_avatar.dart';
+import 'package:mail/models/mail/mail.dart';
+
+class MailDetailScreen extends ResponsiveStatefulWidget {
+  final Mail mail;
+  const MailDetailScreen(this.mail, {super.key});
+
+  @override
+  ResponsiveState<MailDetailScreen> createState() => MailDetailScreenState();
+}
+
+class MailDetailScreenState extends ResponsiveState<MailDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedContainer(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: getSize(context).height * 0.05,
+                ),
+                IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(CupertinoIcons.chevron_back)),
+                SizedBox(height: $constants.insets.sm),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+                  child: AutoSizeText(
+                    maxLines: 1,
+                    widget.mail.getHeader("Subject"),
+                    overflow: TextOverflow.ellipsis,
+                    style: getTextTheme(context).displaySmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+                SizedBox(height: $constants.insets.sm),
+              ],
+            ),
+          ),
+          SizedBox(height: $constants.insets.sm),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsetsGeometry.symmetric(
+                horizontal: $constants.insets.xs,
+              ),
+              child: ElevatedContainer(
+                padding: EdgeInsets.symmetric(
+                    horizontal: $constants.insets.xs,
+                    vertical: $constants.insets.sm),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        MailUserAvatar(
+                            value: widget.mail.getHeader("From"),
+                            read: widget.mail.read),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: $constants.insets.lg),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget buildDesktop(BuildContext context) {
+    return build(context);
+  }
+
+  @override
+  Widget buildMobile(BuildContext context) {
+    return build(context);
+  }
+}
