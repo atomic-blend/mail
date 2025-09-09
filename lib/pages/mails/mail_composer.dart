83ba3d480f018f9be6917bd96619208a8d6826b7
Brandon Guigo <brandon.guigo@gmail.com>
2025-09-09 17:27:12 +0200

    feat: basic version of appflowy editor
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/pages/mails/mail_composer.dart
+++ lib/pages/mails/mail_composer.dart
@@ -2,6 +2,7 @@ import 'package:ab_shared/components/responsive_stateful_widget.dart';
 import 'package:ab_shared/components/widgets/elevated_container.dart';
 import 'package:ab_shared/utils/constants.dart';
 import 'package:ab_shared/utils/shortcuts.dart';
+import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,14 +14,11 @@ class MailComposer extends ResponsiveStatefulWidget {
 }
 
 class _MailComposerState extends ResponsiveState<MailComposer> {
+  EditorState editorState = EditorState.blank(withInitialText: true);
 
   @override
   void initState() {
     super.initState();
-    _initializeEditor();
-  }
-
-  void _initializeEditor() {
   }
 
   @override
@@ -60,7 +58,9 @@ class _MailComposerState extends ResponsiveState<MailComposer> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text("New Mail", style: getTextTheme(context).displaySmall!.copyWith(fontWeight: FontWeight.bold),),
-                    IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.arrow_up_circle_fill, size: 30,color: getTheme(context).primary,),), 
+                    IconButton(onPressed: (){
+                      _sendMail();
+                    }, icon: Icon(CupertinoIcons.arrow_up_circle_fill, size: 30,color: getTheme(context).primary,),), 
                   ],
                 ),
               ),
@@ -72,9 +72,43 @@ class _MailComposerState extends ResponsiveState<MailComposer> {
               SizedBox(height: $constants.insets.xs),
               //TODO: Add email fields (To, Subject, etc.) here
               //TODO: Add email content editor here
+              _buildPaddedDivider(),
+              SizedBox(
+                width: double.infinity,
+                height: 500,
+                child: AppFlowyEditor(
+                  editorStyle: EditorStyle.mobile(), 
+                      editorState: editorState,
+                    ),
+              ),
+
             ],
           ),
         ),
       ) ;
   }
+
+  Widget _buildPaddedDivider() {
+   return  Padding(
+                padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm),
+                child: Divider(),
+              );
+  }
+
+  void _sendMail() {
+    final htmlContent = documentToHTML(editorState.document);
+    final mdContent = documentToMarkdown(editorState.document);
+    final plainTextContent = _plainTextFromMarkdown(mdContent);
+    print("HTML Content: $htmlContent");
+    print("Markdown Content: $mdContent");
+    print("Plain Text Content: $plainTextContent");
+  }
+  
+  String _plainTextFromMarkdown(String mdContent) {
+// Regular expression to match Markdown symbols including '#'
+RegExp regex = RegExp(r'[*_~`#]');
+// Replace Markdown symbols with an empty string
+return mdContent.replaceAll(regex, '');
+  } 
 }
+
