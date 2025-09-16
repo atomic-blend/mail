1bca72b92cca0ed21e18d114ab2b7c52e2bc6e2c
Brandon Guigo <brandon.guigo@gmail.com>
2025-09-16 15:11:46 +0200

    fix: linter
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/pages/mails/views/trashed.dart
+++ lib/pages/mails/views/trashed.dart
@@ -2,10 +2,8 @@ import 'package:ab_shared/components/modals/ab_modal.dart';
 import 'package:ab_shared/components/widgets/elevated_container.dart';
 import 'package:ab_shared/utils/constants.dart';
 import 'package:ab_shared/utils/shortcuts.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mail/blocs/mail/mail_bloc.dart';
 import 'package:mail/pages/mails/filtered_mail.dart';
@@ -16,10 +14,14 @@ class TrashedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FilteredMailScreen(
-      filterFunction: (mails) => mails?.where((mail) => mail.trashed == true).toList() ?? [],
+      filterFunction: (mails) =>
+          mails?.where((mail) => mail.trashed == true).toList() ?? [],
       header: ElevatedContainer(
         border: Border.all(color: getTheme(context).error),
-        padding: EdgeInsets.symmetric(horizontal: $constants.insets.sm, vertical: $constants.insets.xs + $constants.insets.xxs,),
+        padding: EdgeInsets.symmetric(
+          horizontal: $constants.insets.sm,
+          vertical: $constants.insets.xs + $constants.insets.xxs,
+        ),
         width: double.infinity,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -31,15 +33,34 @@ class TrashedScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("xxx mails trashed", style: getTextTheme(context).bodyMedium!.copyWith(fontWeight: FontWeight.bold)),
-                  Text("Those are the mails that have been trashed by you, they will be deleted after 30 days.", style: getTextTheme(context).bodyMedium!.copyWith(color: Colors.grey.shade600,)),
+                  Text("xxx mails trashed",
+                      style: getTextTheme(context)
+                          .bodyMedium!
+                          .copyWith(fontWeight: FontWeight.bold)),
+                  Text(
+                      "Those are the mails that have been trashed by you, they will be deleted after 30 days.",
+                      style: getTextTheme(context).bodyMedium!.copyWith(
+                            color: Colors.grey.shade600,
+                          )),
                   SizedBox(height: $constants.insets.xs),
-                  GestureDetector(onTap: () {
-                    showDialog(context: context, builder: (context) => ABModal(title: "Delete all trashed mails", description: "Are you sure you want to delete all trashed mails?", warning: "This action cannot be undone.", onConfirm: () {
-                      context.read<MailBloc>().add(EmptyTrash());
-                      Navigator.of(context).pop();
-                    }));
-                  }, child: Text("Delete all trashed mails", style: getTextTheme(context).bodyMedium!.copyWith(color: getTheme(context).error, fontWeight: FontWeight.bold))),
+                  GestureDetector(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) => ABModal(
+                                title: "Delete all trashed mails",
+                                description:
+                                    "Are you sure you want to delete all trashed mails?",
+                                warning: "This action cannot be undone.",
+                                onConfirm: () {
+                                  context.read<MailBloc>().add(EmptyTrash());
+                                  Navigator.of(context).pop();
+                                }));
+                      },
+                      child: Text("Delete all trashed mails",
+                          style: getTextTheme(context).bodyMedium!.copyWith(
+                              color: getTheme(context).error,
+                              fontWeight: FontWeight.bold))),
                 ],
               ),
             )
