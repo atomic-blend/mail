456552dba62f97e7b538c7dbff7ff74fbef5df97
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-04 10:10:14 +0200

    feat: add actions on bulk edit list
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/pages/mails/selected_list.dart
+++ lib/pages/mails/selected_list.dart
@@ -2,7 +2,9 @@ import 'package:ab_shared/utils/constants.dart';
 import 'package:ab_shared/utils/shortcuts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ab_shared/components/buttons/ab_button.dart';
 import 'package:mail/components/cards/mail_card.dart';
+import 'package:mail/i18n/strings.g.dart';
 import 'package:mail/models/mail/mail.dart';
 
 class SelectedListScreen extends StatelessWidget {
@@ -37,6 +39,32 @@ class SelectedListScreen extends StatelessWidget {
                     .copyWith(color: Colors.grey.shade600),
               ),
               SizedBox(height: $constants.insets.sm),
+              Row(
+                spacing: $constants.insets.xs,
+                children: [
+                  _buildActionButton(
+                      context,
+                      context.t.mail_actions.mark_as_read,
+                      CupertinoIcons.envelope_open, () {
+                    print("Mark as read");
+                  }),
+                  _buildActionButton(
+                      context,
+                      context.t.mail_actions.mark_as_unread,
+                      CupertinoIcons.envelope, () {
+                    print("Mark as unread");
+                  }),
+                  _buildActionButton(context, context.t.mail_actions.archive,
+                      CupertinoIcons.archivebox, () {
+                    print("Archive");
+                  }),
+                  _buildActionButton(context, context.t.mail_actions.trash,
+                      CupertinoIcons.trash, () {
+                    print("Trash");
+                  }),
+                ],
+              ),
+              SizedBox(height: $constants.insets.sm),
               ...mails.map((mail) => Padding(
                     padding: EdgeInsets.only(bottom: $constants.insets.xs),
                     child: MailCard(
@@ -49,6 +77,16 @@ class SelectedListScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildActionButton(
+      BuildContext context, String label, IconData icon, VoidCallback onTap) {
+    return ABButton(
+      label: label,
+      icon: icon,
+      onTap: onTap,
+      iconSize: 15,
     );
   }
 }
