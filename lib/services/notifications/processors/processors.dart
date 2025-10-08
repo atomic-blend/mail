2494bb924594d7c70779df55f37cf8d825e57d36
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-08 12:11:02 +0200

    feat: add notifications for received emails
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/services/notifications/processors/processors.dart
+++ lib/services/notifications/processors/processors.dart
@@ -1,8 +1,14 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:mail/services/notifications/processors/received_mail_processor.dart';
 
 class Processors {
   static processAndNotify(RemoteMessage message) async {
+    print('Processing message: ${message.data['type']}');
     switch (message.data['type']) {
+      case 'MAIL_RECEIVED':
+        print('Received mail: ${message.data['subject']}');
+        await ReceivedMailProcessor.process(message);
+        break;
       default:
         throw UnimplementedError(
             'No processor found for payload type: ${message.data['payload_type']}');
