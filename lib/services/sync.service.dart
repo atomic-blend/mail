294eb40b084b706ea968445a427b8602e97a940a
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-17 15:05:39 +0200

    fix: support new cloud sync status
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/services/sync.service.dart
+++ lib/services/sync.service.dart
@@ -1,4 +1,6 @@
 import 'package:ab_shared/blocs/auth/auth.bloc.dart';
+import 'package:ab_shared/components/app/ab_header.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mail/blocs/mail/mail_bloc.dart';
@@ -21,5 +23,30 @@ class SyncService {
 
     // Sync user data
     context.read<AuthBloc>().add(const RefreshUser());
+  }
+
+  static List<SyncedElement> getSyncedElements({
+    required MailState mailState,
+    required AuthState authState,
+  }) {
+    return [
+      SyncedElement(
+          key: const Key("mails"),
+          label: "Mails",
+          icon: CupertinoIcons.mail,
+          count: mailState.mails?.length ?? 0),
+      SyncedElement(
+          key: const Key("drafts"),
+          label: "Drafts",
+          icon: CupertinoIcons.square_pencil,
+          count: mailState.drafts?.length ?? 0),
+    ];
+  }
+
+  static bool isSyncing({
+    required MailState mailState,
+    required AuthState authState,
+  }) {
+    return mailState is MailLoading || authState is Loading;
   }
 }
