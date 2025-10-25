f753d3d47dbff41cab7202b54d2043c3dcd6e18e
Brandon Guigo <brandon.guigo@gmail.com>
2025-10-25 20:20:15 +0200

    fix: add paywall route
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 92 (46 per locale)
 ///
-/// Built on 2025-10-10 at 15:13 UTC
+/// Built on 2025-10-24 at 16:39 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
